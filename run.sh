d2ed301d09c1400cb6a95c24d6eb0f62ba887569
Erik Xu <81636759@qq.com>
2023-12-20 04:18:17 +0800

    修改端口.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker rm -f wx3rd-api
-docker run --name wx3rd-api -d -p 80:5000 wx3rd-api:latest
+docker run --name wx3rd-api -d -e ASPNETCORE_URLS="http://*:80" -p 80:80 wx3rd-api:latest
